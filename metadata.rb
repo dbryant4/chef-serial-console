5a50968dc51cc29b4f2cd929fb9faed6d3c87c15
Derrick Bryant <dbryant4@gmail.com>
2013-04-26 10:40:39 -0400

    Updated metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             "serial-console"
-maintainer       "YOUR_NAME"
-maintainer_email "YOUR_EMAIL"
-license          "All rights reserved"
+maintainer       "Derrick Bryant"
+maintainer_email "dbryant4@gmail.com"
+license          "Apache 2.0"
 description      "Installs/Configures serial-console"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
