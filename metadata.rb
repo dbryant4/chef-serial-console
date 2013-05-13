dc5347ea6a09c194566d40424df402751f14e2fa
Derrick Bryant <dbryant4@gmail.com>
2013-05-12 22:33:50 -0400

    comments out splashimage and hiddenmenu lines in grub.conf

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email "dbryant4@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures serial-console"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.4"
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -20,14 +20,45 @@
 case node.platform
 when 'centos', 'redhat'
   # Handle grub serial port
-  ruby_block "comment_out_grub_splashimage" do
+  #ruby_block "comment_out_grub_splashimage" do
+  #  block do
+  #    file = Chef::Util::FileEdit.new("/boot/grub/grub.conf")
+  #    file.insert_line_if_no_match("/^(splashimage.*)$/", "##{1}")
+  #    file.write_file
+  #  end
+  #end 
+
+  # Remove hiddenline from grub.conf
+  execute "comment_out_hiddenmenu" do
+    command "sed -i '/^hiddenmenu/s/^/#/' /boot/grub/grub.conf"
+    action :run
+    only_if "grep ^hiddenmenu /boot/grub/grub.conf"
+  end 
+  
+  # Remove splash from grub.conf
+  execute "comment_out_splashimage" do
+    command "sed -i '/^splash/s/^/#/' /boot/grub/grub.conf"
+    action :run
+    only_if "grep ^splash /boot/grub/grub.conf"
+  end 
+
+  # Add serial line to grub 
+  #ruby_block "insert_grub_serial_line" do
+  #  block do
+  #    file = Chef::Util::FileEdit.new("/boot/grub/grub.conf")
+  #    file.insert_line_after_match("/^timeout/", "serial --unit=0 --speed=#{node[:serial_console][:tty]} --word=8 --parity=no --stop=1")
+  #    file.write_file
+  #  end
+  #end 
+  
+  # Add terminal line to grub 
+  ruby_block "insert_grub_terminal_line" do
     block do
       file = Chef::Util::FileEdit.new("/boot/grub/grub.conf")
-      file.insert_line_if_no_match("/^(splashimage.*)$/", "##{1}")
+      file.insert_line_after_match("/^timeout/", "terminal --timeout=10 serial console")
       file.write_file
     end
   end 
-
 
   # Start tty
   execute "start-serial-console" do
